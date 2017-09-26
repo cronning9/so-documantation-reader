a055c58c8214be86114a426d4c01dace1bc9e773
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-09-26 19:48:11 -0400

    initial commit

---
+++ test/so_docs_reader/core_test.clj
@@ -0,0 +1,7 @@
+(ns so-docs-reader.core-test
+  (:require [clojure.test :refer :all]
+            [so-docs-reader.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
